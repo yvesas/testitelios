bde0b608288d25ae763b0a00969ca6d4a649d39f
Yves Siqueira <yves.as@gmail.com>
2020-06-08 19:37:55 -0300

    show images

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Running INDEX...")
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use('/', router);
+app.listen(3001);
+console.log("Running on http://localhost:3001")
